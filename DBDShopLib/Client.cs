fce4036c95887818506ed4783467a95f11fe4aba
Jon Allende Sanz <jallende014@ikasle.ehu.eus>
2019-10-28 11:59:19 +0100

    Corrected minor mistake on DeleteProdcuts ast to not get an error, but still to be revised

--- DBDShopLib/Client.cs
+++ DBDShopLib/Client.cs
@@ -62,7 +62,7 @@ namespace DBDShopLib
         {
             foreach(Product product in products)
             {
-                string query = "DELETE FROM Products WHERE Id =" + product.Id + ";";
+                string query = "DELETE FROM Products WHERE Id =" + product.idproducto + ";";
                 MySqlCommand cmd = new MySqlCommand(query, m_connection);
                 cmd.ExecuteNonQuery();
             }
