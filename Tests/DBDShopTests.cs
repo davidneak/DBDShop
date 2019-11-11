88be497dcbab7f82d140828a7df9c037ff408fb0
David Negrete <dnegrete001@ikasle.ehu.eus>
2019-11-11 21:11:12 +0100

    some test

--- Tests/DBDShopTests.cs
+++ Tests/DBDShopTests.cs
@@ -28,13 +28,94 @@ namespace Tests
             Assert.IsTrue(products.Count == 2);
         }
         [TestMethod]
-        public void MyOhterTest()
+        public void ConnectionBDTest()
+		{
+			m_connection = new MySqlConnection();
+			m_connection.ConnectionString =
+			"Server=myremotesql.com ;" +
+			"database= NW0HSO5HO7;" +
+			"UID=NW0HSO5HO7; " +
+			"password=ZEALzol3dN;";
+            //Check the connection
+			Assert.IsTrue(m_connection.Open()=true);
+		}
+        [TestMethod]
+        public void InsertProductTest()
         {
-            //Connect to the test database
-            //Client client = new Client("NLphb4HrH0", "NLphb4HrH0", "VM8GYV3qZ7");
-            
-            //Any testing you need to do
-            //....
-        }
+			//Connect to the test database
+			Client client = new Client("NW0HSO5HO7", "NW0HSO5HO7", "ZEALzol3dN");
+			//Get all the existing products
+			List<Product> products = client.GetProducts();
+			//Delete all the products
+			client.DeleteProducts(products);
+			//Insert a product
+			client.insertProduct(24, "Manzana", 3);
+			//Connect to BD
+			m_connection = new MySqlConnection();
+			m_connection.ConnectionString =
+			"Server=myremotesql.com ;" +
+			"database= NW0HSO5HO7;" +
+			"UID=NW0HSO5HO7; " +
+			"password=ZEALzol3dN;";
+			m_connection.Open() = true;
+
+            //Get product from BD
+			string query = "SELECT * FROM producto";
+			MySqlCommand cmd = new MySqlCommand(query, m_connection);
+			MySqlDataReader reader = cmd.ExecuteReader();
+			while (reader.Read())
+			{
+                //Check if is the same product
+				Assert.IsTrue(int.Parse(reader.GetValue(0).ToString())=24);
+				Assert.IsTrue(reader.GetValue(1).ToString()="Manzana");
+				Assert.IsTrue(int.Parse(reader.GetValue(2).ToString())=3);
+				
+			}
+			reader.Close();
+			
+
+		}
+        [TestMethod]
+        public void SetPriceTest()
+		{
+			//Connect to the test database
+			Client client = new Client("NW0HSO5HO7", "NW0HSO5HO7", "ZEALzol3dN");
+			//Connect to BD
+			m_connection = new MySqlConnection();
+			m_connection.ConnectionString =
+			"Server=myremotesql.com ;" +
+			"database= NW0HSO5HO7;" +
+			"UID=NW0HSO5HO7; " +
+			"password=ZEALzol3dN;";
+			m_connection.Open() = true;
+
+			//Get product from BD
+			string query = "INSERT INTO producto_pedido VALUES (23, 12, 3, 6.25)";
+			MySqlCommand cmd = new MySqlCommand(query, m_connection);
+			cmd.ExecuteNonQuery();
+
+			//Set price
+			client.modificarPrecio(3.58, 23 );
+
+			//Check if were correctly modify
+			// Get product from BD
+			string query = "SELECT precio FROM producto_pedido WHERE producto = 23 AND pedido = 12";
+			MySqlCommand cmd = new MySqlCommand(query, m_connection);
+			MySqlDataReader reader = cmd.ExecuteReader();
+			while (reader.Read())
+			{
+				//Check if is the same price
+				Assert.IsTrue(float.Parse(reader.GetValue(0).ToString())=3.58);
+				
+
+			}
+			reader.Close();
+
+			//Delete test product
+			string query = "DELETE FROM producto_pedido WHERE producto=23 AND pedido=12";
+			MySqlCommand cmd = new MySqlCommand(query, m_connection);
+			cmd.ExecuteNonQuery();
+
+		}
     }
 }
